d11de9209a029f12a13d19affff74edfbad453fb
philipduan <philip_du_an@hotmail.com>
2017-10-17 11:51:01 -0400

    Initial Setup

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "f99d28b3eb634e8d87c41e4f6b3a2af5"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
